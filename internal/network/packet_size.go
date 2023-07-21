31c4fe4f0a88b914c862434c1cf04d33866273cb
Ranya Sharma <ranyasharma2004@gmail.com>
2023-07-21 10:19:45 -0500

    adding welford

--- internal/network/packet_size.go
+++ internal/network/packet_size.go
@@ -2,17 +2,35 @@ package network
 
 import (
 	"encoding/json"
+	"errors"
+	"math"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/traffic-refinery/traffic-refinery/internal/network"
+	"github.com/traffic-refinery/traffic-refinery/internal/welford"
 )
 
 type PacketSize struct {
-	Size  int64
+	Size  welford.Welford
 }
 
 // Calculates the size of a packet 
 
-func (c *PacketSize) AddPacket(pkt *network.Packet) error {
+func (c *PacketSize) LengthPacket(pkt *network.Packet) error {
 	c.Size = pkt.Length
 	return nil
+}
+
+type PacketSizeOut struct {
+	SizeAvg    float64
+	SizeVar    float64
+}
+
+// Collect returns a []byte representation of the counter
+func (c *PacketSize) Collect() []byte {
+	b, _ := json.Marshal(PacketSizeOut{
+		SizeAvg:    c.Size.Avg,
+		SizeVar:    c.Size.Var,
+	})
+	return b
 }
